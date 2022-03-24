e54ae84265c38f0da2b87bac4fd06e90232c8330
Thiag0Oliveira <contato.thg@hotmail.com>
2022-03-24 16:19:57 -0300

    Relacionamentos das tabelas do Projeto

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from apps.funcionario.models import Funcionario
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100)
+    pertente = models.ForeignKey(Funcionario,on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
