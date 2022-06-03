09da2ea2f8e089aa81f278bb2b47738c0eabe489
Thiag0Oliveira <contato.thg@hotmail.com>
2022-06-03 17:41:31 -0300

    Adicionando ip AWS

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-unflapo60nw1y+(g^7b-q!ka@f*%=f__)ffcyfnu22chp+&*yt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.227.227.197']
 
 
 # Application definition
